8560b0c8b7514a995082318f58f54558f17e4da7
Maaz Patel <maazsajid.patel@einfochips.com>
2025-09-24 12:57:39 +0530

    added getJobConfig function

--- vars/jobUtils.groovy
+++ vars/jobUtils.groovy
@@ -7,8 +7,21 @@ class JobUtils implements Serializable {
     def listJobs() {
         return Jenkins.instance.getAllItems(Job.class).collect { it.fullName }
     }
+
+    @NonCPS
+    def getJobConfig(String jobName) {
+        def job = Jenkins.instance.getItemByFullName(jobName, Job.class)
+        if (job) {
+            return job.getConfigFile().asString()   // returns the raw XML
+        }
+        return null
+    }
 }
 
 def listJobs() {
     return new JobUtils().listJobs()
+}
+
+def getJobConfig(String jobName) {
+    return new JobUtils().getJobConfig(jobName)
 }
